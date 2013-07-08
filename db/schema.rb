a677e62e87624ea7da6f751b7aac758b8f080c05
Artyom Smirnov <smirnov@balticit.ru>
2013-07-08 14:33:44 +0300

    Remove localization

--- db/schema.rb
+++ db/schema.rb
@@ -71,20 +71,10 @@ ActiveRecord::Schema.define(:version => 20130704125027) do
 
   add_index "feedbacks", ["email"], :name => "index_feedbacks_on_email"
 
-  create_table "page_translations", :force => true do |t|
-    t.integer  "page_id"
-    t.string   "locale"
-    t.text     "content"
-    t.text     "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "page_translations", ["locale"], :name => "index_page_translations_on_locale"
-  add_index "page_translations", ["page_id"], :name => "index_page_translations_on_page_id"
-
   create_table "pages", :force => true do |t|
+    t.string   "name"
     t.string   "slug"
+    t.text     "content"
     t.boolean  "hidden",     :default => false
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
@@ -98,22 +88,12 @@ ActiveRecord::Schema.define(:version => 20130704125027) do
   add_index "pages", ["seo_id"], :name => "index_pages_on_seo_id"
   add_index "pages", ["slug"], :name => "index_pages_on_slug", :unique => true
 
-  create_table "seo_translations", :force => true do |t|
-    t.integer  "seo_id"
-    t.string   "locale"
+  create_table "seos", :force => true do |t|
     t.string   "title"
     t.string   "keywords"
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-  end
-
-  add_index "seo_translations", ["locale"], :name => "index_seo_translations_on_locale"
-  add_index "seo_translations", ["seo_id"], :name => "index_seo_translations_on_seo_id"
-
-  create_table "seos", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
--- db/seeds/pages.rb
+++ db/seeds/pages.rb
@@ -1,5 +1,9 @@
 #coding: utf-8
 
 Page.reset_column_information
-FactoryGirl.create(:page, slug: 'index')
-FactoryGirl.create(:page, slug: 'contacts')
+FactoryGirl.create(:page, slug: 'index', name: 'Главная')
+FactoryGirl.create(:page, slug: 'our_services', name: 'Наши услуги ')
+FactoryGirl.create(:page, slug: 'order_of_services', name: 'Заказ услуг')
+FactoryGirl.create(:page, slug: 'help_for_clients', name: 'В помощь клиенту')
+FactoryGirl.create(:page, slug: 'leave_a_comment', name: 'Оставить отзыв')
+FactoryGirl.create(:page, slug: 'contacts', name: 'Контакты')
