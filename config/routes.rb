ee0ddaf7fc43b734a3fc126a481dd7c617990e8e
Artyom Smirnov <smirnov@balticit.ru>
2013-07-08 16:03:48 +0300

    Remove last appendixes of localization

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,15 @@ Uraspect::Application.routes.draw do
 
   mount_roboto
 
-  root :to => "pages#show", :slug => 'index', locale:'en'
+  root :to => "pages#show", :slug => 'index'
 
   mount Ckeditor::Engine => '/ckeditor'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :feedbacks, :only => :create
+  get ':slug' => 'pages#show', :as => :slug
 
-  localized do
-    root :to => "pages#show", :slug => 'index'
-    get ':slug' => 'pages#show', :as => :slug
-    resources :pages
-  end
+  resources :pages
+  resources :feedbacks, :only => :create
 end
