12cfec907070a632af87939b600f902ce3939527
Nastya <nastya@Lemon.(none)>
2013-07-11 10:35:12 +0300

    pr

--- app/controllers/jurisprudences_controller.rb
+++ app/controllers/jurisprudences_controller.rb
@@ -4,7 +4,7 @@ class JurisprudencesController < ApplicationController
   end
 
   def show
-    @jurisprudence = Jurisprudence.find_by_id(params[:id])
+    @jurisprudence = Jurisprudence.find(params[:id])
   end
 
 end
