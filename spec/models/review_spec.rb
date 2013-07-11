803dafa14f21f8656c4399a6f5cc9bd2bade76b6
Artyom Smirnov <smirnov@balticit.ru>
2013-07-11 10:57:24 +0300

    Add rspec tests for review model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Review do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:review) { FactoryGirl.build(:review) }
+
+  it 'should be valid with valid attributes' do
+    review.should be_valid
+  end
+
+  describe '#caption' do
+    it 'should not be empty' do
+      review.caption = ' '
+      review.should be_invalid
+    end
+  end
+
+  describe '#content' do
+    it 'should not be empty' do
+      review.content = ' '
+      review.should be_invalid
+    end
+  end
+
 end
