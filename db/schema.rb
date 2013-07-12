6d5c93005d299bdb01b1432fe8fb0ea33f55f792
Artyom Smirnov <smirnov@balticit.ru>
2013-07-12 12:38:42 +0300

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710122941) do
+ActiveRecord::Schema.define(:version => 20130712093549) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -101,6 +101,14 @@ ActiveRecord::Schema.define(:version => 20130710122941) do
     t.boolean  "moderated",  :default => false
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
+  end
+
+  create_table "sample_docs", :force => true do |t|
+    t.string   "category"
+    t.string   "name"
+    t.string   "file_source"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "seos", :force => true do |t|
