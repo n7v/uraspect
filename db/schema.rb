49bce2ec02cdd7248c6186cd349af4b42ddfaeaa
Artyom Smirnov <smirnov@balticit.ru>
2013-07-12 12:59:35 +0300

    Migrate (2)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130712093549) do
+ActiveRecord::Schema.define(:version => 20130712095731) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -61,6 +61,20 @@ ActiveRecord::Schema.define(:version => 20130712093549) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
+
+  create_table "doc_categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "docs", :force => true do |t|
+    t.integer  "doc_category_id"
+    t.string   "name"
+    t.string   "file_source"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
   create_table "feedbacks", :force => true do |t|
     t.string   "email"
