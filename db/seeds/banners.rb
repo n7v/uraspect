da243414afc4667c52b6987b90d4af32bc418b4f
Nastya <nastya@Lemon.(none)>
2013-07-16 12:37:21 +0300

    banners

---
+++ db/seeds/banners.rb
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+Banner.reset_column_information
+Banner.create!(
+  content: '<span class="paragraph_white">Оформи</span> <span class="paragraph_yellow">страховой</span>
+           <span class="paragraph_yellow">полис</span> <span class="paragraph_white">на выгодных</span>
+           <span class="paragraph_white">условиях</span>',
+  url: '/'
+)
