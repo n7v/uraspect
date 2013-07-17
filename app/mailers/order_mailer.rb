179e54070a4476a6008c281034366a8f7f29b342
Nastya <nastya@Lemon.(none)>
2013-07-17 15:57:01 +0300

    pr

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,8 +3,8 @@ class OrderMailer < ActionMailer::Base
   ADMINS = AdminUser.all.map(&:email) 
 	 
    def order_of_services(order)
+	   @first_name = order.first_name
 	   @last_name = order.last_name
-	   @last_name = order.first_name
 	   @phon = order.phone
 	   @email = order.email
 	   @content = order.content
