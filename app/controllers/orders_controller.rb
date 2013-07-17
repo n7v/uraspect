f89c59198c26664b15020677a428b1d9f9d8496b
Nastya <nastya@Lemon.(none)>
2013-07-17 17:45:41 +0300

    pr

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,11 +4,11 @@ class OrdersController < ApplicationController
     @order = Order.new(params[:order])
  	    if @order.save
         OrderMailer.order_of_services(@order).deliver
-      	redirect_to thanks_path(order_id: @order.id)
+        redirect_to thanks_path(order_id: @order.id)
       else
-      	@page = Page.find_by_slug('order_of_services')
-      	render 'pages/order_of_services'
-    	end
+        @page = Page.find_by_slug('order_of_services')
+        render 'pages/order_of_services'
+      end
   end
 
   def thanks_order
