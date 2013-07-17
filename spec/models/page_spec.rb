f3e03b36d7d592bf8d129693cef5ec6ddec583fe
novoselov <novsem@gmail.com>
2013-07-17 14:42:10 +0300

    tests: fix specs from template

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -33,33 +33,11 @@ describe Page do
     end
 
     context 'when slug blank' do
-      context 'when there is only russian translation' do
-        it 'should be filled with transliterated name' do
-          rus = FactoryGirl.build(:page_translation, locale: :ru, name: 'Всероссийская ассоциация ежей')
-          page.translations = [rus]
-          page.slug = ' '
-          page.should be_valid
-          page.slug.should == 'vserossiyskaya-assotsiatsiya-ezhey'
-        end
-      end
-
-      context 'when there is english translation' do
-        it 'should be filled with parameterized english name' do
-          eng = FactoryGirl.build(:page_translation, locale: :en, name: 'Russian hedgehogs assosiation')
-          rus = FactoryGirl.build(:page_translation, locale: :ru, name: 'Всероссийская ассоциация ежей')
-          page.translations = [eng, rus]
-          page.slug = ' '
-          page.should be_valid
-          page.slug.should == 'russian-hedgehogs-assosiation'
-        end
-      end
-
-      context 'when there are not translations' do
-        it 'should be invalid' do
-          page.translations = []
-          page.slug = ' '
-          page.should have(1).errors_on(:slug)
-        end
+      it 'should be filled with transliterated name' do
+        page.name = 'Всероссийская ассоциация ежей'
+        page.slug = ' '
+        page.should be_valid
+        page.slug.should == 'vserossiyskaya-assotsiatsiya-ezhey'
       end
     end
 
