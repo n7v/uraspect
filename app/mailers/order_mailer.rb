39c3054927e5733a7f5b524711665ad7c7ced50b
Nastya <nastya@Lemon.(none)>
2013-07-18 15:07:41 +0300

    pravka testa and probel

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,15 +1,15 @@
 #coding: utf-8
 class OrderMailer < ActionMailer::Base
+
+  def order_of_services(order)
+    @first_name = order.first_name
+    @last_name = order.last_name
+    @phone = order.phone
+    @email = order.email
+    @content = order.content
+
+    mail :to => AdminUser.all.map(&:email),
+         :subject => "Заказ услуг"
+  end
 	 
-   def order_of_services(order)
-     admins = AdminUser.all.map(&:email) 
-	   @first_name = order.first_name
-	   @last_name = order.last_name
-	   @phone = order.phone
-	   @email = order.email
-	   @content = order.content
-	  
-	   mail :to => admins,
-	        :subject => "Заказ услуг"
-	 end
 end
