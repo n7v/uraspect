608e8da322f1fc435a7b7c157f1c0c5b85938aff
Nastya <nastya@Lemon.(none)>
2013-07-18 10:40:37 +0300

    pr

--- db/seeds/banners.rb
+++ db/seeds/banners.rb
@@ -2,8 +2,8 @@
 
 Banner.reset_column_information
 Banner.create!(
-  content: '<span class="paragraph_white">Оформи</span> <span class="paragraph_yellow">страховой</span>
-           <span class="paragraph_yellow">полис</span> <span class="paragraph_white">на выгодных</span>
-           <span class="paragraph_white">условиях</span>',
+  content: '<span class="paragraph_white">Калькулятор</span> 
+           <span class="paragraph_yellow">госпошлины</span>
+           <span class="paragraph_white"></span>',
   url: '/'
 )
