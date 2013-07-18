fbccc1177463658bab28d63bbd0ae10c08a448ef
Nastya <nastya@Lemon.(none)>
2013-07-18 11:34:19 +0300

    test

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,15 +1,15 @@
 #coding: utf-8
 class OrderMailer < ActionMailer::Base
-  ADMINS = AdminUser.all.map(&:email) 
 	 
    def order_of_services(order)
+     admins = AdminUser.all.map(&:email) 
 	   @first_name = order.first_name
 	   @last_name = order.last_name
-	   @phon = order.phone
+	   @phone = order.phone
 	   @email = order.email
 	   @content = order.content
 	  
-	   mail :to => ADMINS,
+	   mail :to => admins,
 	        :subject => "Заказ услуг"
 	 end
 end
