bf25bd14923b1e0d803f5cfbaf79e5dd377f1dcf
novoselov <novsem@gmail.com>
2013-07-18 11:48:04 +0300

    search: remove old code

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -5,35 +5,10 @@ module PageHelper
     raw truncate_html object.content, length: 200, omission: "..."
   end
 
-  def search_title_for(model)
-    model_names = {
-      :doc            => "образцам документов",
-      :jurisprudence  => "судебным практикам",
-      :page           => "страницам"
-    }
-    "Результаты поиска по #{model_names[model]}"
-  end
-
   def menu_item_state(page)
     slug = params[:slug]
     if page.slug == slug || page.children.find{|p| p.slug == slug }
       'current'
     end
   end
-
-  def excerpt(result, str)
-    result[:excerpter].excerpt! str
-  end
-
-  def search_object_path(model, object)
-    case model
-    when :jurisprudence
-      jurisprudence_path(object)
-    when :doc
-      doc_load_path(object)
-    when :page
-      slug_path(object)
-    end
-  end
-
 end
